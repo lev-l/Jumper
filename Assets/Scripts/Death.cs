9e0f6d5dc6691103fda26c8201c9f26723e88313
Leo <balashov.leo@gmail.com>
2021-06-20 12:17:52 +0300

    Add death by spears

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class Death : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.GetComponent<Movement>())
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
