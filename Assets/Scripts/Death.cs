36d46c1e44d0299bd85ee0b32ea5d8ba9d888ec4
Leo <balashov.leo@gmail.com>
2021-07-02 12:54:56 +0300

    Add jumping from spears

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -9,10 +9,33 @@ public class DeadMenu
 
 public class Death : MonoBehaviour
 {
+    private Collider2D _collider;
+    private ContactFilter2D _filter;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+
+        _filter.useTriggers = false;
+        _filter.layerMask = Physics2D.GetLayerCollisionMask(gameObject.layer);
+        _filter.useLayerMask = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         UnityMovement movement = collision.GetComponent<UnityMovement>();
         if (movement)
+        {
+            StartCoroutine(Dead(movement));
+        }
+    }
+
+    private IEnumerator Dead(UnityMovement movement)
+    {
+        Collider2D[] colliders = new Collider2D[4];
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        if (_collider.OverlapCollider(_filter, colliders) > 0)
         {
             movement.enabled = false;
             movement.GetComponent<Rope>().enabled = false;
