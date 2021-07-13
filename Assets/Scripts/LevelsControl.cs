9d668a7751485a056ffb300f7168712db0fc4b47
Leo <balashov.leo@gmail.com>
2021-07-13 10:28:43 +0300

    Add transitions between levels and saves

---
+++ Assets/Scripts/LevelsControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization;
+
+[DataContract]
+public class LevelsControl
+{
+    [DataMember]
+    public List<string> OpenedLevels;
+    [DataMember]
+    public string CurrentLevel;
+
+    public LevelsControl()
+    {
+        OpenedLevels = new List<string>(5);
+        CurrentLevel = SceneManager.GetActiveScene().name;
+    }
+
+    public bool OpenLevel(string level)
+    {
+        if (OpenedLevels.Contains(level))
+        {
+            return false;
+        }
+        else
+        {
+            OpenedLevels.Add(level);
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelsControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelsControler : MonoBehaviour
+{
+    public string SaveFileName;
+    public LoadingPosition StartPosition;
+    private LevelsControl _levels;
+    private string _path;
+
+    private void Start()
+    {
+        _path = "./Assets/Resourses/" + SaveFileName + ".json";
+
+        _levels = (LevelsControl)JsonUtility.FromJson
+                                                (
+                                                File.ReadAllText(_path),
+                                                typeof(LevelsControl)
+                                                );
+        _levels.OpenedLevels.Add(SceneManager.GetActiveScene().name);
+        File.WriteAllText(_path, JsonUtility.ToJson(_levels));
+
+        GameObject.FindGameObjectWithTag("Player").transform.position = StartPosition.LoadPosition;
+    }
+}
